0e2bb85f5c384a7708cb491cb2998fab2afaacac
ralphsz288 <rakph28@gmail.com>
2023-09-27 19:05:09 +0300

    added training session screen

---
+++ Projects/shared_preferences_data/lib/screens/sessions_screen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences_data/data/session.dart';
+import 'package:shared_preferences_data/data/sp_helper.dart';
+
+class SessionsScreen extends StatefulWidget {
+  const SessionsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SessionsScreen> createState() => _SessionsScreenState();
+}
+
+class _SessionsScreenState extends State<SessionsScreen> {
+  List<Session> sessions = [];
+  final TextEditingController txtDescription = TextEditingController();
+  final TextEditingController txtDuration = TextEditingController();
+  final SPHelper helper = SPHelper();
+
+  @override
+  void initState() {
+    helper.init().then((value) {updateScreen();});
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Training Sessions'),
+      ),
+      body: ListView(children: getContent()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showSessionDialog(context);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Future <dynamic> showSessionDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Insert Training Session'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    controller: txtDescription,
+                    decoration: const InputDecoration(hintText: 'Description'),
+                  ),
+                  TextField(
+                    controller: txtDuration,
+                    decoration: const InputDecoration(hintText: 'Duration'),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    txtDuration.text = '';
+                    txtDescription.text = '';
+                  },
+                  child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                  onPressed: saveSession,
+                  child: const Text('Save'),
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+  Future saveSession() async {
+    DateTime now = DateTime.now();
+    String today = '${now.year}-${now.month}-${now.day}';
+    int id = helper.getCounter() + 1;
+    Session newSession = Session(id, today, txtDescription.text, int.tryParse(txtDuration.text) ?? 0);
+    helper.writeSession(newSession).then((_){
+      updateScreen();
+      helper.setCounter();
+    });
+    txtDuration.text = '';
+    txtDescription.text = '';
+    Navigator.of(context).pop();
+  }
+
+  List<Widget> getContent() {
+    List<Widget> tiles = [];
+    sessions.forEach((session) {
+      tiles.add(ListTile(
+        title: Text(session.description),
+        subtitle: Text('${session.date} - duration: ${session.duration} min'),
+      ));
+    });
+    return tiles;
+  }
+
+  void updateScreen() {
+    sessions = helper.getSessions();
+    setState(() {});
+  }
+
+
+}
